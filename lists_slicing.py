4c2f6141be67fe76119051dd2503cdb756483aa8
Starship100 <jimmy.widlund@outlook.com>
2026-02-01 23:36:50 +0100

    slice string

--- lists_slicing.py
+++ lists_slicing.py
@@ -3,3 +3,14 @@ alphabet = "abcdefghijklmnopqrstuvwxyzåäö"
 print(alphabet[0:10:3])
 print(alphabet[:5:2])
 print(alphabet[12:17])
+
+# Räkna ut summan av talen i strängen
+# split - separerar strängen
+# strip - tar bort whitespace
+data = "10, 12, 20, 30, 40"  # ska bli 112
+token_list = data.split(",")  # gör sträng till lista
+sum_so_far = 0
+for token in token_list:
+    token_number = int(token.strip()) # tar bort mellanslag
+    sum_so_far = sum_so_far + token_number
+print(sum_so_far)
