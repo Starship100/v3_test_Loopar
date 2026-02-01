bc6c106d67fb31a66c80851b603d8b607a2ea58f
Starship100 <jimmy.widlund@outlook.com>
2026-02-01 15:25:19 +0100

    while loopar

---
+++ main.py
@@ -0,0 +1,22 @@
+""""
+r = 1
+while r > 0:
+    print(r)
+    r = r - 1
+    print("Inuti loopen")
+
+print("Efter loopen")
+
+for i in range(5):
+    print(i)
+"""""
+
+user_input = ""
+while user_input != "q":
+    print("Press 'q' to quit.")
+    user_input = input("< ")
+
+print("Outside the loop.")
+
+
+
